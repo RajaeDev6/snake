0dec772c963bbde206a0a14cc4eda11299df4d6d
rajae <rgivans9@gmail.com>
2025-12-31 00:42:17 -0500

    feat: Grow snake when eat food and end game when collide with border

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snake
+
+Classic Snake game written in c using ncurses library
+
+Right now i am just writing code to get the basics out the way then i refactor
--- main.c
+++ main.c
@@ -1,84 +1,134 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <ncurses.h>
 #include <unistd.h>
 #include <time.h>
 
+#define CELL 10
+#define MAX_SNAKE_SIZE (CELL * CELL)
+#define INITIAL_HEIGHT 20
+#define INITIAL_WIDTH 49
+#define INITIAL_SNAKE_X 2
+#define INITIAL_SNAKE_Y 2
+#define INITIAL_SPEED 120000  // microseconds per frame
+#define BORDER 1              // border width
+
+typedef struct {
+    int x;
+    int y;
+} Segment;
+
 int main() {
-	srand(time(NULL));
+    srand(time(NULL));
 
-	initscr();
-	/* setlocale(LC_ALL, "en_US.UTF-8"); */
-	cbreak();
-	noecho();
-	curs_set(0);
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    refresh();
 
-	refresh();   
-	int Height = 20;
-	int Width = 49;
+    const int height = INITIAL_HEIGHT;
+    const int width = INITIAL_WIDTH;
 
-	int snake_x = 1;
-	int snake_y = 1;
+    // snake setup
+    Segment snake[MAX_SNAKE_SIZE];
+    int snake_len = 1;
+    snake[0].x = INITIAL_SNAKE_X;
+    snake[0].y = INITIAL_SNAKE_Y;
 
-	WINDOW *win = newwin(Height, Width, 5, 10);
-	nodelay(win, true);
+    int dx = 1;  // initial movement to the right
+    int dy = 0;
 
-	int dx = 0;
-	int dy = 0;
-	int running = 1;
+    // Apple setup
+    int apple_x = (rand() % (width - 2 * BORDER)) + BORDER;
+    int apple_y = (rand() % (height - 2 * BORDER)) + BORDER;
 
-	int apple_x = rand() % Width - 2;
-	int apple_y = rand() % Height - 2;
+    int running = 1;
+    const useconds_t FPS = INITIAL_SPEED;
 
-	int FPS = 100000;
+    WINDOW *win = newwin(height, width, 5, 10);
+    nodelay(win, true);
+    keypad(win, true);
 
-	while (running) {
-		char c = wgetch(win);
-		
-		snake_x += dx;
-		snake_y += dy;
+    while (running) {
+        // Input
+        int c = wgetch(win);
+        switch (c) {
+            case 's':  
+                if (dy == 0) { dx = 0; dy = 1; }
+                break;
+            case 'w':  
+                if (dy == 0) { dx = 0; dy = -1; }
+                break;
+            case 'd':  
+                if (dx == 0) { dx = 1; dy = 0; }
+                break;
+            case 'a':  
+                if (dx == 0) { dx = -1; dy = 0; }
+                break;
+            case 'q':
+                running = 0;
+                break;
+        }
 
-		switch (c) {
-			case 'j':
-				dy = 1;
-				dx = 0;
-				FPS = 150500;
-				break;
-			case 'k':
-				dy = -1;
-				dx = 0;
-				FPS = 150500;
-				break;
-				break;
-			case 'l':
-				dx = 1;
-				dy = 0;
-				FPS = 100000;
-				break;
-				break;
-			case 'h':
-				dx = -1;
-				dy = 0;
-				FPS = 100000;
-				break;
-				break;
-			case 'q':
-				running = 0;
-				break;
-		}
+        // move body: from tail to head
+        for (int i = snake_len - 1; i > 0; i--) {
+            snake[i] = snake[i - 1];
+        }
 
-		if (snake_y == apple_y && snake_x == apple_x) {
-			apple_y = rand() % Height - 2;
-			apple_x = rand() % Width - 2;
-		}
+        // move head
+        snake[0].x += dx;
+        snake[0].y += dy;
 
-		werase(win);
-		box(win, 0, 0);
+        // check collision with borders
+        if (snake[0].x < BORDER || snake[0].x >= width - BORDER ||
+            snake[0].y < BORDER || snake[0].y >= height - BORDER) {
+            break;
+        }
 
-		mvwaddch(win, apple_y, apple_x, 'x');
-		mvwaddch(win, snake_y, snake_x, '0');
+        // check collision with self
+        for (int i = 1; i < snake_len; i++) {
+            if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+                running = 0;
+            }
+        }
 
-		usleep(FPS);
-	}
+        // check apple collision
+        if (snake[0].x == apple_x && snake[0].y == apple_y) {
+            if (snake_len < MAX_SNAKE_SIZE) {
+                snake_len++;
+            }
+            // place new apple in free space
+            int conflict;
+            do {
+                conflict = 0;
+                apple_x = (rand() % (width - 2 * BORDER)) + BORDER;
+                apple_y = (rand() % (height - 2 * BORDER)) + BORDER;
+                for (int i = 0; i < snake_len; i++) {
+                    if (snake[i].x == apple_x && snake[i].y == apple_y) {
+                        conflict = 1;
+                        break;
+                    }
+                }
+            } while (conflict);
+        }
 
-	endwin();
+        // Update 
+        werase(win);
+        box(win, 0, 0);
+
+        mvwaddch(win, apple_y, apple_x, 'x');
+
+        for (int i = 0; i < snake_len; i++) {
+            mvwaddch(win, snake[i].y, snake[i].x, '0');
+        }
+
+        wrefresh(win);
+        usleep(FPS);
+    }
+
+    endwin();
+    printf("Game Over! Your score: %d\n", snake_len - 1);
+    return 0;
 }
+
